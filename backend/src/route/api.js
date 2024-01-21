e303564156e9074d242dc46af8d73fa3505b0070
Dicky <dickyferdy5@gmail.com>
2024-01-21 10:39:09 +0700

    add address API

--- backend/src/route/api.js
+++ backend/src/route/api.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
+import addressController from "../controller/address-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 
 
@@ -10,6 +11,13 @@ userRouter.use(authMiddleware);
 userRouter.get("/api/v1/users", userController.get);
 userRouter.patch("/api/v1/users", userController.update);
 userRouter.delete("/api/v1/logout", userController.logout);
+
+// Address API
+userRouter.post("/api/v1/addresses", addressController.create);
+userRouter.get("/api/v1/addresses", addressController.list);
+userRouter.get("/api/v1/addresses/:addressId", addressController.get);
+userRouter.put("/api/v1/addresses/:addressId", addressController.update);
+userRouter.delete("/api/v1/addresses/:addressId", addressController.remove);
 
 export {
   userRouter
