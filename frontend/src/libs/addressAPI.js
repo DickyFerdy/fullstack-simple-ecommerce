5f59a2df1b048665316965828135c8df4c4a6212
Dicky <dickyferdy5@gmail.com>
2024-01-21 10:43:15 +0700

    create addressAPI libs

---
+++ frontend/src/libs/addressAPI.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+
+const APIBaseUrl = 'http://localhost:5000/api/v1';
+
+const getAllUserAddressAPI = async (token) => {
+  const response = await axios.get(`${APIBaseUrl}/addresses`, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token || ""}`,
+    },
+  });
+
+  return response;
+};
+
+const createAddressAPI = async (token, formData) => {
+  const response = await axios.post(`${APIBaseUrl}/addresses`, formData, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token || ""}`
+    }
+  });
+
+  return response;
+};
+
+const getAddressByIdAPI = async (token, id) => {
+  const response = await axios.get(`${APIBaseUrl}/addresses/${id}`, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token || ""}`
+    }
+  });
+
+  return response;
+};
+
+const deleteAddressByIdAPI = async (token, id) => {
+  const response = await axios.delete(`${APIBaseUrl}/addresses/${id}`, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token || ""}`
+    }
+  });
+
+  return response;
+};
+
+const updateAddressByIdAPI = async (token, id, formData) => {
+  const response = await axios.put(`${APIBaseUrl}/addresses/${id}`, formData, {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token || ""}`
+    }
+  });
+
+  return response;
+};
+
+export {
+  getAllUserAddressAPI,
+  createAddressAPI,
+  getAddressByIdAPI,
+  deleteAddressByIdAPI,
+  updateAddressByIdAPI
+}
