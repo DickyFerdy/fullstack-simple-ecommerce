353f0e39dcf769313fe768b997ce9af846397eb4
Dicky <dickyferdy5@gmail.com>
2024-01-23 07:34:24 +0700

    loading validation

--- frontend/src/app/profile/address/update/[id]/page.jsx
+++ frontend/src/app/profile/address/update/[id]/page.jsx
@@ -11,13 +11,13 @@ import Button from "@/components/ui/button";
 import Label from "@/components/ui/label";
 import Input from "@/components/ui/input";
 import TextArea from "@/components/ui/textarea";
-import { FileX } from "lucide-react";
-import Link from "next/link";
+import NotFound from "@/components/notFound";
+import Image from "next/image";
 
 
 const Page = ({ params: { id } }) => {
   const { push } = useRouter();
-
+  const [loading, setLoading] = useState(true);
   const [token, setToken] = useState('');
   const [address, setAddress] = useState({
     title: '',
@@ -78,8 +78,10 @@ const Page = ({ params: { id } }) => {
             country: response.data.data.country,
             postal_code: response.data.data.postal_code,
           });
+          setLoading(false);
         } catch (err) {
           console.log(err.message);
+          setLoading(false);
         }
       };
 
@@ -88,81 +90,71 @@ const Page = ({ params: { id } }) => {
   }, [token]);
 
   return (
-    <div className="flex h-max bg-white flex-col md:flex-row md:overflow-hidden">
-      <Sidebar />
-      {
-        address.title !== '' ?
-        <div className="flex h-full md:ml-64 md:w-full flex-col md:flex-row px-3 py-4 md:px-2">
-          <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-            <MainTitle title={"Shipping Address"} />
-            <div className="flex flex-col md:grid md:grid-cols-6 gap-6">
-              <div className="md:col-span-4 lg:col-span-3 xl:col-span-4 flex flex-col gap-6">
-                <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm">
-                  <ContentTitle title={"Update Shipping Address"} />
-                  <div className="p-6 pt-0">
-                    <form className="grid gap-4">
-                      <div className="grid gap-2">
-                        <Label name={"Title"} />
-                        <Input id={"title"} type={"text"} name={"title"} placeholder={address.title} onChange={handleChange} />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label name={"Street"} />
-                        <TextArea id={"street"} name={"street"} placeholder={address.street} onChange={handleChange} />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label name={"City"} />
-                        <Input id={"city"} type={"text"} name={"city"} placeholder={address.city} onChange={handleChange} />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label name={"Province"} />
-                        <Input id={"province"} type={"text"} name={"province"} placeholder={address.province} onChange={handleChange} />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label name={"Country"}/>
-                        <Input id={"country"} type={"text"} name={"country"} placeholder={address.country} onChange={handleChange} />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label name={"Postal Code"} />
-                        <Input id={"postal_code"} type={"number"} name={"postal_code"} placeholder={address.postal_code} onChange={handleChange} />
-                      </div>
-                      <div className="text-center text-red-500 text-sm font-medium">
-                        {error ? (
-                          <p>{error}</p>
-                        ) : (
-                          <p>
-                            <br></br>
-                          </p>
-                        )}
-                      </div>
-                      <div>
-                        <Button name={"Submit"} handle={handleSubmit} />
-                      </div>
-                    </form>
+    <>
+      { loading ? (
+        <div className="flex w-screen h-screen items-center justify-center bg-white">
+          <Image src={"/loading.svg"} width='0' height='0' alt="loading" style={{ width: 'auto', height: 'auto' }} />
+        </div>
+      ) : address.title.trim() !== '' ? (
+        <div className="flex h-max bg-white flex-col md:flex-row md:overflow-hidden">
+          <Sidebar />
+          <div className="flex h-full md:ml-64 md:w-full flex-col md:flex-row px-3 py-4 md:px-2">
+            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+              <MainTitle title={"Shipping Address"} />
+              <div className="flex flex-col md:grid md:grid-cols-6 gap-6">
+                <div className="md:col-span-4 lg:col-span-3 xl:col-span-4 flex flex-col gap-6">
+                  <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm">
+                    <ContentTitle title={"Update Shipping Address"} />
+                    <div className="p-6 pt-0">
+                      <form className="grid gap-4">
+                        <div className="grid gap-2">
+                          <Label name={"Title"} />
+                          <Input id={"title"} type={"text"} name={"title"} placeholder={address.title} onChange={handleChange} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label name={"Street"} />
+                          <TextArea id={"street"} name={"street"} placeholder={address.street} onChange={handleChange} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label name={"City"} />
+                          <Input id={"city"} type={"text"} name={"city"} placeholder={address.city} onChange={handleChange} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label name={"Province"} />
+                          <Input id={"province"} type={"text"} name={"province"} placeholder={address.province} onChange={handleChange} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label name={"Country"}/>
+                          <Input id={"country"} type={"text"} name={"country"} placeholder={address.country} onChange={handleChange} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label name={"Postal Code"} />
+                          <Input id={"postal_code"} type={"number"} name={"postal_code"} placeholder={address.postal_code} onChange={handleChange} />
+                        </div>
+                        <div className="text-center text-red-500 text-sm font-medium">
+                          {error ? (
+                            <p>{error}</p>
+                          ) : (
+                            <p>
+                              <br></br>
+                            </p>
+                          )}
+                        </div>
+                        <div>
+                          <Button name={"Submit"} handle={handleSubmit} />
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </main>
-        </div> :
-        <div className="flex h-screen md:ml-64 md:w-full flex-col md:flex-row px-3 py-4 md:px-2">
-          <main className="flex flex-1 flex-col gap-4 md:gap-8">
-            <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-              <FileX size={64} className="mb-4 text-black" />
-              <h1 className="text-4xl text-center font-bold text-gray-900">Oops! Page not found.</h1>
-              <p className="mt-2 text-center text-gray-600">
-                The page you are looking for doesn't exist or has been moved.
-              </p>
-              <Link
-                className="inline-flex h-10 items-center justify-center rounded-md bg-blue-600 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 mt-8"
-                href="/"
-                >
-                Go back home
-              </Link>
-            </div>
-          </main>
+            </main>
+          </div>
         </div>
-      }
-    </div>
+      ) : (
+        <NotFound />
+      )}
+    </>
   )
 }
 
--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -10,8 +10,10 @@ import { AddressList } from "@/components/addresses";
 import { getAllUserAddressAPI } from "@/libs/addressAPI";
 import MainTitle from "@/components/ui/title/mainTitle";
 import ContentTitle from "@/components/ui/title/contentTitle";
+import Image from "next/image";
 
 const Page = () => {
+  const [loading, setLoading] = useState(true);
   const [token, setToken] = useState('');
   const [address, setAddress] = useState([]);
   const [data, setData] = useState({
@@ -39,8 +41,10 @@ const Page = () => {
             username: response.data.data.username,
             email: response.data.data.email,
           });
+          setLoading(false);
         } catch (err) {
           console.log(err.message);
+          setLoading(false);
         }
       };
 
@@ -59,76 +63,84 @@ const Page = () => {
   }, [token]);
 
   return (
-    <div className="flex h-max md:h-screen bg-white flex-col md:flex-row md:overflow-hidden">
-      <Sidebar />
-      <div className="flex h-full md:ml-64 md:w-full flex-col md:flex-row px-3 py-4 md:px-2">
-        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-          <MainTitle title={"User Profile"} />
-          <div className="flex flex-col md:grid md:grid-cols-6 gap-6">
-            <div className="md:col-span-4 lg:col-span-3 xl:col-span-4 flex flex-col gap-6">
-              <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm">
-                <ContentTitle title={"Profile Information"} />
-                <div className="p-6 pt-0">
-                  <div className="flex items-center gap-2">
-                    <div className="relative flex shrink-0 overflow-hidden rounded-full h-16 w-16 justify-center items-center">
-                      <CircleUserRound size={56} strokeWidth={1.3} />
-                      {/* <Image className="aspect-square h-full w-full" alt="@shadcn" src="" /> */}
-                      {/* <div className="flex h-full w-full items-center justify-center rounded-full bg-muted">JP</div> */}
-                    </div>
-                    <div className="grid gap-1 text-slate-700">
-                      <h3 className="font-semibold">{data.name}</h3>
-                      <p className="text-sm leading-none">
-                        {data.username}
-                      </p>
-                      <p className="text-sm leading-none">
-                        {data.email}
-                      </p>
+    <>
+      { loading ? (
+        <div className="flex w-screen h-screen items-center justify-center bg-white">
+          <Image src={"/loading.svg"} width='0' height='0' alt="loading" style={{ width: 'auto', height: 'auto' }} />
+        </div>
+      ) : (
+        <div className="flex h-max md:h-screen bg-white flex-col md:flex-row md:overflow-hidden">
+          <Sidebar />
+          <div className="flex h-full md:ml-64 md:w-full flex-col md:flex-row px-3 py-4 md:px-2">
+            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+              <MainTitle title={"User Profile"} />
+              <div className="flex flex-col md:grid md:grid-cols-6 gap-6">
+                <div className="md:col-span-4 lg:col-span-3 xl:col-span-4 flex flex-col gap-6">
+                  <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm">
+                    <ContentTitle title={"Profile Information"} />
+                    <div className="p-6 pt-0">
+                      <div className="flex items-center gap-2">
+                        <div className="relative flex shrink-0 overflow-hidden rounded-full h-16 w-16 justify-center items-center">
+                          <CircleUserRound size={56} strokeWidth={1.3} />
+                          {/* <Image className="aspect-square h-full w-full" alt="@shadcn" src="" /> */}
+                          {/* <div className="flex h-full w-full items-center justify-center rounded-full bg-muted">JP</div> */}
+                        </div>
+                        <div className="grid gap-1 text-slate-700">
+                          <h3 className="font-semibold">{data.name}</h3>
+                          <p className="text-sm leading-none">
+                            {data.username}
+                          </p>
+                          <p className="text-sm leading-none">
+                            {data.email}
+                          </p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-end gap-2">
-              <div className="grid gap-2">
-                <Link href={'/profile/update/information'} title="Update Information" className="flex items-center justify-center bg-blue-600 transition-colors hover:bg-blue-500 text-white text-sm h-9 rounded-md px-6">Update Profile</Link>
-              </div>
-              <div className="grid gap-2">
-                <Link href={'/profile/update/password'} title="Update Password" className="flex items-center justify-center bg-blue-600 transition-colors hover:bg-blue-500 text-white text-sm h-9 rounded-md px-6">Update Password</Link>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm pb-6">
-              <div className="flex justify-between space-y-1.5 p-6">
-                <div className="text-2xl font-semibold">
-                  Shipping Address
-                </div>
-                <div>
-                  {
-                    address.data?.data?.length >= 4 ?
-                    null :
-                    <div className="hover:scale-125 transition-all">
-                      <Link href={'/profile/address'} title="Add New Address">
-                        <Plus size={24}/>
-                      </Link>
-                    </div>
-                  }
-                </div>
-              </div>
-              <div className="px-4">
-                {
-                  address.data?.data?.length >= 1 ?
-                  <AddressList api={address}/> :
-                  <div className="flex items-center justify-center">
-                    <p>You don't have a shipping address</p>
+                <div className="flex flex-col justify-end gap-2">
+                  <div className="grid gap-2">
+                    <Link href={'/profile/update/information'} title="Update Information" className="flex items-center justify-center bg-blue-600 transition-colors hover:bg-blue-500 text-white text-sm h-9 rounded-md px-6">Update Profile</Link>
                   </div>
-                }
+                  <div className="grid gap-2">
+                    <Link href={'/profile/update/password'} title="Update Password" className="flex items-center justify-center bg-blue-600 transition-colors hover:bg-blue-500 text-white text-sm h-9 rounded-md px-6">Update Password</Link>
+                  </div>
+                </div>
               </div>
-            </div>
+              <div>
+                <div className="rounded-lg text-slate-700 border border-slate-300 shadow-sm pb-6">
+                  <div className="flex justify-between space-y-1.5 p-6">
+                    <div className="text-2xl font-semibold">
+                      Shipping Address
+                    </div>
+                    <div>
+                      {
+                        address.data?.data?.length >= 4 ?
+                        null :
+                        <div className="hover:scale-125 transition-all">
+                          <Link href={'/profile/address'} title="Add New Address">
+                            <Plus size={24}/>
+                          </Link>
+                        </div>
+                      }
+                    </div>
+                  </div>
+                  <div className="px-4">
+                    {
+                      address.data?.data?.length >= 1 ?
+                      <AddressList api={address}/> :
+                      <div className="flex items-center justify-center">
+                        <p>You don't have a shipping address</p>
+                      </div>
+                    }
+                  </div>
+                </div>
+              </div>
+            </main>
           </div>
-        </main>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
