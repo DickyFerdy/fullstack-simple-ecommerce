4c739578ad96ee11781729acda36a99b68dfe81d
Dicky <dickyferdy5@gmail.com>
2024-01-28 15:17:09 +0700

    category route

--- backend/src/route/api.js
+++ backend/src/route/api.js
@@ -3,6 +3,7 @@ import userController from "../controller/user-controller.js";
 import addressController from "../controller/address-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 import productController from "../controller/product-controller.js";
+import categoryController from "../controller/category-controller.js";
 
 
 const userRouter = new express.Router();
@@ -24,6 +25,9 @@ userRouter.delete("/api/v1/addresses/:addressId", addressController.remove);
 userRouter.post("/api/v1/products", productController.create);
 userRouter.get("/api/v1/products", productController.search);
 userRouter.get("/api/v1/products/category/:categoryName", productController.category);
+
+// Category API
+userRouter.get("/api/v1/categories", categoryController.get);
 
 export {
   userRouter
