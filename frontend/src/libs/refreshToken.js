6d71890ef815477a185d3a765f65b7fc895f6ebb
Dicky <dickyferdy5@gmail.com>
2024-01-31 10:19:34 +0700

    refreshTokenAPI libs

--- frontend/src/libs/refreshToken.js
+++ frontend/src/libs/refreshToken.js
@@ -4,8 +4,8 @@ const refreshToken = async () => {
   const response = await axios.get(`http://localhost:5000/api/v1/token`, {
     withCredentials: true,
   });
-  const token = await response.data.data.accessToken;
-  return token;
+  
+  return response.data.data;
 }
 
 export {
