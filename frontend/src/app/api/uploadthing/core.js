7bdddd3c6b99e22dd8696bc226bf671ac6dc435a
Dicky <dickyferdy5@gmail.com>
2024-02-07 21:10:40 +0700

    create uploadthing api route

---
+++ frontend/src/app/api/uploadthing/core.js
@@ -0,0 +1,16 @@
+import { createUploadthing, UploadThingError } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "2MB" } })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      return { uploadedBy: metadata.userId };
+    }),
+};
+ 
+export const OurFileRouter = ourFileRouter;
