0692912fd81392a93d9f1b6035152c48b8e02df2
Dicky <dickyferdy5@gmail.com>
2024-02-07 21:07:08 +0700

    register components

---
+++ frontend/src/components/register/index.jsx
@@ -0,0 +1,58 @@
+import Button from "@/components/ui/button";
+import Label from "@/components/ui/label";
+import Input from "@/components/ui/input";
+
+
+const Register = ({ handleSubmit, handleChange, error }) => {
+  return (
+    <div className="w-full h-max md:h-screen bg-gray-50" >
+      <main className="flex flex-col justify-center items-center h-full py-10 px-4 sm:px-6 lg:px-8">
+        <div className="rounded-lg border bg-[#2d3547] shadow-sm w-full max-w-md mx-auto space-y-6">
+          <div className="space-y-1.5 p-4">
+            <h2 className="mt-6 text-center text-4xl font-extrabold text-[#e0e0e0]">
+              Register
+            </h2>
+          </div>
+          <div className="p-6 pt-0">
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <div>
+                <Label name={"Name"} />
+                <Input id={"name"} type={"text"} name={"name"} placeholder={"John Doe"} onChange={handleChange} />
+              </div>
+              <div>
+                <Label name={"Username"} />
+                <Input id={"username"} type={"text"} name={"username"} placeholder={"johndoe"} onChange={handleChange} />
+              </div>
+              <div>
+                <Label name={"Email address"} />
+                <Input id={"email-address"} type={"email"} name={"email"} placeholder={"johndoe@mail.com"} onChange={handleChange} />
+              </div>
+              <div>
+                <Label name={"Password"} />
+                <Input id={"password"} type={"password"} name={"password"} placeholder={"password"} onChange={handleChange} />
+              </div>
+              <div className="text-center text-red-500 text-base font-medium">
+                {
+                  error ? <p>{error}</p> : <p><br></br></p>
+                }
+              </div>
+              <div>
+                <Button name={"Register"} />
+              </div>
+              <div className="flex items-center justify-center text-base">
+                <p className="font-medium text-[#e0e0e0] mr-1">
+                  Already have an account?
+                </p>
+                <a className="font-medium text-blue-600 hover:text-blue-500 underline" href="/login">
+                  Log In
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Register;
