44d3be1442b192dc8dd879abe8624f4f577a5502
Dicky <dickyferdy5@gmail.com>
2024-02-07 21:12:31 +0700

    add component

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -5,9 +5,7 @@ import { useRouter } from 'next/navigation';
 import { usePathname } from "next/navigation";
 import { refreshToken } from "@/libs/refreshToken";
 import loginAPI from '@/libs/loginAPI';
-import Button from "@/components/ui/button";
-import Label from "@/components/ui/label";
-import Input from "@/components/ui/input";
+import Login from "@/components/login";
 
 
 const Page = () => {
@@ -57,45 +55,7 @@ const Page = () => {
 
 
   return (
-    <div className="w-full h-screen bg-gray-50" >
-      <main className="flex flex-col justify-center items-center h-full py-10 px-4 sm:px-6 lg:px-8">
-        <div className="rounded-lg border bg-[#2d3547] shadow-sm w-full max-w-md mx-auto space-y-6">
-          <div className="space-y-1.5 p-4">
-            <h2 className="mt-6 text-center text-4xl font-extrabold text-[#e0e0e0]">
-              Log In
-            </h2>
-          </div>
-          <div className="p-6 pt-0">
-            <form className="space-y-4" onSubmit={handleSubmit}>
-              <div>
-                <Label name={"Username"} />
-                <Input id={"username"} type={"text"} name={"username"} placeholder={"johndoe"} onChange={handleChange} />
-              </div>
-              <div>
-                <Label name={"Password"} />
-                <Input id={"password"} type={"password"} name={"password"} placeholder={"password"} onChange={handleChange} />
-              </div>
-              <div className="text-center text-red-500 text-sm font-medium">
-                {
-                  error ? <p>{error}</p> : <p><br></br></p>
-                }
-              </div>
-              <div>
-                <Button name={"Login"} />
-              </div>
-              <div className="flex items-center justify-center text-base">
-                <p className="font-medium text-[#e0e0e0] mr-1">
-                  Don't have an account?
-                </p>
-                <a className="font-medium text-blue-600 hover:text-blue-500 underline" href="register">
-                  Create Account
-                </a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </main>
-    </div>
+    <Login handleSubmit={handleSubmit} handleChange={handleChange} error={error} />
   );
 };
 
